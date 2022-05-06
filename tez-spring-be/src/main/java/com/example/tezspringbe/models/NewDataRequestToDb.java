992ea775d83c1beede099b9e9404a8d351686507
mete.kodal <metehankodal@gmail.com>
2022-05-06 20:12:13 +0300

    new data request backend services

---
+++ tez-spring-be/src/main/java/com/example/tezspringbe/models/NewDataRequestToDb.java
@@ -0,0 +1,10 @@
+package com.example.tezspringbe.models;/**
+*@Author meteh
+*@create 6.05.2022 20:09
+*//*
+ * @created 05
+ * @author meteh
+ */
+public class NewDataRequestToDb {
+}
+
---
+++ tez-spring-be/src/main/java/com/example/tezspringbe/repos/NewDataRequestRepo.java
@@ -0,0 +1,5 @@
+package com.example.tezspringbe.repos;/**
+*@Author meteh
+*@create 6.05.2022 20:10
+*/public interface NewDataRequestRepo {
+}
