23f27e24278b4499b4bdd5976635bbfd28c2b111
mete.kodal <metehankodal@gmail.com>
2022-05-20 17:43:43 +0300

    getDataRequest

--- tez-spring-be/src/main/java/com/example/tezspringbe/repos/DatasetRepo.java
+++ tez-spring-be/src/main/java/com/example/tezspringbe/repos/DatasetRepo.java
@@ -2,10 +2,15 @@ package com.example.tezspringbe.repos;
 
 import com.example.tezspringbe.models.Dataset;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 /**
  * @Author meteh
  * @create 6.05.2022 20:10
  */
 public interface DatasetRepo extends MongoRepository<Dataset,String> {
+    @Query("{onaylandiMi:onaylanmadi}")
+    List<Dataset> getAllOnaylanmadi(String onaylandimi);
 }
